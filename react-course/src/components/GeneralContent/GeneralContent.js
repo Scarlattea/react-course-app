d3eb5bd567942ceb0a9ea8a253dc9f87c2f4bd2d
Scarlattea <scarlattea@gmail.com>
2023-04-25 15:21:09 +0200

    about me page

--- react-course/src/components/GeneralContent/GeneralContent.js
+++ react-course/src/components/GeneralContent/GeneralContent.js
@@ -5,9 +5,18 @@ export default function GeneralContent(props){
     return(
         <div className="portfolio-general-content">
            <div className="about-container">
-                <div className="corner-top-left"></div>
-                <div className="corner-bottom-right"></div>
+            <div className="corner-wrapper top"><div className="corner-top-left"></div></div>
+            <div className="corner-wrapper bottom"><div className="corner-bottom-right"></div></div>
+            <div className="name-container">Zsófia Székely</div>
+            <div className="job-container">Frontend Developer</div>
+            <div className="short-introduction">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend sed est luctus tincidunt. Nullam eget sodales enim, ut dictum nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin faucibus diam nec iaculis ultricies. Ut at volutpat nibh. Suspendisse ut mauris faucibus turpis mattis convallis. Sed tortor lacus, mollis quis purus nec, mollis hendrerit justo. Maecenas feugiat viverra erat, a finibus massa tristique et.
+
+                Phasellus et lorem vel justo sollicitudin dignissim in non arcu. Vestibulum volutpat, nisl ut tempus pellentesque, massa ligula maximus nisi, eget lacinia justo purus id sapien. Nunc efficitur leo elit, id vulputate nisi posuere at. Aenean risus turpis, ullamcorper ac nisi a, ornare malesuada libero. Nulla aliquam auctor risus, quis venenatis urna posuere vitae. Pellentesque bibendum erat ac augue pretium accumsan. Suspendisse in ex pellentesque, bibendum metus a, dignissim diam. Quisque non euismod lectus. Aenean scelerisque nisi condimentum neque viverra dapibus. Cras nec purus ante. Vivamus ultricies lacus nibh, et viverra libero mattis vitae. Vestibulum scelerisque ullamcorper accumsan. Vestibulum feugiat ut mi facilisis venenatis.
+
+                    Aenean eu iaculis eros. Vestibulum sit amet tristique massa. Donec semper lorem at porta pulvinar. Aliquam dapibus at ligula non commodo. Vivamus et dui vestibulum neque facilisis commodo sit amet ac sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent sollicitudin ipsum vitae urna hendrerit, sed tristique est venenatis. Morbi turpis dui, convallis fringilla dui id, congue feugiat lectus. Mauris eget enim et lectus iaculis condimentum ut quis nisi.</div>
+                    <div className="check-my-work-button">My portfolio</div>
            </div>
+           
         </div>
     );
 }
